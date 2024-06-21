8f42e1427008390fb6756211ef553d4ebc9fc6a4
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-06-21 09:15:14 -0400

    Print list of achievemetns and its status

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ if __name__ == "__main__":
     achievement_name, achievements_code = scraper.extract_achievements()
     world_achievements: list = get_world_achievements.get_completed_achievements()
 
-    for name, achievement in zip(achievement_name, achievements_code):
-        print(
-            f"Name: {name} Code: {achievement} Completed: {achievement in world_achievements}"
-        )
-
-#    for achievement in world_achievements:
-#        print(f"code: {achievement}")
-#
+for name, achievement in zip(achievement_name, achievements_code):
+    print(
+        f"Name: {name} Code: {achievement} Completed: {achievement in world_achievements}"
+    )
