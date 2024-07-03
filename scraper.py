6b720ab6567ade6c19ba7c5bf3b5398c7f32415d
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-07-03 17:38:29 -0400

    filter contries by continent

--- scraper.py
+++ scraper.py
@@ -21,9 +21,10 @@ def fetch_page_content():
     return response
 
 
-def extract_achievements():
+def get_achievements():
     names = []
     achievements = []
+    imgs = []
     keywords = ["story", "nether", "end", "adventure", "husbandry"]
     response = fetch_page_content()
     soup = BeautifulSoup(response.content, "html.parser")
@@ -32,12 +33,17 @@ def extract_achievements():
     for element in elements:
         code_tags = element.find_all("code")
         b_tags = element.find_all("b")
+        img_tags = element.find_all("img")
 
         filter_code_tags = [item for item in code_tags if "#" not in item.get_text()]
 
-        for name, code in zip(b_tags, filter_code_tags):
+        for name, code, img in zip(b_tags, filter_code_tags, img_tags):
             names.append(name.get_text())
+            imgs.append("https://minecraft.wiki/" + img["src"])
             if code.get_text():
                 achievements.append("minecraft:" + code.get_text())
 
-    return names, achievements
+    return names, achievements, imgs
+
+
+print(get_achievements())
